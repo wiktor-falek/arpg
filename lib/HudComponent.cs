d6865827c50515092fcee0b00b410e5df01e88c5
qbibubi <58397100+qbibubi@users.noreply.github.com>
2025-01-02 14:40:20 +0100

    renamed: menu graphics -> hud

--- lib/HudComponent.cs
+++ lib/HudComponent.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 // @TODO: This will be a separate class for UI and displaying menus
 //        and other things
-public class Menu
+public class HUD
 {
 
 }
