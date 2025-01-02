23ed225c28813a5c0ebb2d21784748c04b7f958b
wiktor-falek <falekwiktor@gmail.com>
2025-01-02 10:12:10 +0100

    move files to lib/

--- lib/Projectile.cs
+++ lib/Projectile.cs
@@ -3,14 +3,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-public interface IEntity
-{
-  string Id { get; set; }
-  void LoadAssets(ContentManager contentManager);
-  void Update(GameTime gameTime);
-  void Draw(SpriteBatch spriteBatch, GraphicsDevice device);
-}
-
 public class Projectile : IEntity
 {
   public string Id { get; set; } = Guid.NewGuid().ToString();
