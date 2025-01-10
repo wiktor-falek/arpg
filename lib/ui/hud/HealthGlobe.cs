38b71ff6e3388deccb70fadda751d04666046501
wiktor-falek <falekwiktor@gmail.com>
2025-01-10 22:53:09 +0100

    adjust health globe

--- lib/ui/hud/HealthGlobe.cs
+++ lib/ui/hud/HealthGlobe.cs
@@ -7,29 +7,31 @@ public class HealthGlobe : IHudElement
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
 
-    // 285 is temporary value and has to be dynamically changed based on current resolution
-    public Vector2 Position { get; set; } = new Vector2(0, 285);
-    public Vector2 Size { get; set; } = new Vector2(75, 75);
-    public Rectangle Rectangle
-    {
-        get => new((int)Position.X, (int)Position.Y, (int)Size.X, (int)Size.Y);
-    }
-
     public int Health { get; set; }
     public int MaxHealth { get; set; }
+
+    public Vector2 Size { get; set; }
+    public Vector2 Position { get; set; }
+    public Rectangle Rectangle => new((int)Position.X, (int)Position.Y, (int)Size.X, (int)Size.Y);
+
+    public HealthGlobe()
+    {
+        Size = new Vector2(55, 55);
+        Position = new Vector2(0, 360 - Size.Y);
+    }
 
     public void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
     {
         var globeTexture = new Texture2D(graphicsDevice, 1, 1);
-        globeTexture.SetData([Color.Red]);
-        spriteBatch.Draw(globeTexture, Rectangle, Color.Red);
+        globeTexture.SetData([new Color(150, 10, 10)]);
+        spriteBatch.Draw(globeTexture, Rectangle, Color.White);
 
-        string healthGlobeValuesText = $"{Health} / {MaxHealth}";
+        string healthGlobeValuesText = $"{Health}0/{MaxHealth}0";
         spriteBatch.DrawString(
             Assets.Fonts.MonogramExtened,
             healthGlobeValuesText,
             new Vector2(Position.X, Position.Y - 12),
-            Color.Red,
+            Color.White,
             0.0f,
             Vector2.Zero,
             1f,
